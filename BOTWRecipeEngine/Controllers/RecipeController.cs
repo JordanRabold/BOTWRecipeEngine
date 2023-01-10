ad0da589978b72ef02259a1ed0889daf2af48cb0
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2023-01-10 11:40:20 -0800

    Created the recipe controller

---
+++ BOTWRecipeEngine/Controllers/RecipeController.cs
@@ -0,0 +1,24 @@
+﻿using BOTWRecipeEngine.Data;
+using BOTWRecipeEngine.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BOTWRecipeEngine.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly RecipeContext _context;
+
+        public RecipeController(RecipeContext context)
+        {
+            _context = context;
+        }
+
+        // GET
+        [HttpGet]
+        public IActionResult GetRecipe(int id)
+        {
+            return View();
+        }
+    }
+}

bac7a0208bd7d42390f45ad9b47f4be264c9ee7e
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2023-01-10 11:40:08 -0800

    Created a Recipe class

---
+++ BOTWRecipeEngine/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BOTWRecipeEngine.Models
+{
+    public class Recipe
+    {
+        public int ID { get; set; }
+
+        public string RecipeName { get; set; }
+
+        public string IngredientName { get; set; } // TODO: Determine how to handle multiple ingredients
+
+        public string Hearts { get; set; }
+
+        public string RecipePicture { get; set; }
+    }
+}

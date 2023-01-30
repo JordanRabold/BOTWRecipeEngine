dc2193e3d1c5b6c24967c32c492e0c780b1044f0
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2023-01-30 10:46:07 -0800

    Added SQL Database

---
+++ SQLQuery1.sql
@@ -0,0 +1,35 @@
+DROP DATABASE DBBOTWRecipeEngine
+
+CREATE DATABASE DBBOTWRecipeEngine
+
+USE DBBOTWRecipeEngine
+
+CREATE TABLE Botw_Recipes(
+	RecipeName VARCHAR(40),
+	Ingredient1 VARCHAR(40),
+	Ingredient2 VARCHAR(40),
+	Ingredient3 VARCHAR(40),
+	Ingredient4 VARCHAR(40),
+	Num_Hearts FLOAT,
+	Recipe_Image VARBINARY(max)
+);
+
+INSERT INTO Botw_Recipes 
+	VALUES('Pumpkin Stew', 
+		   'Fortified Pumpkin',
+		   'Goat Butter',
+		   'Fresh Milk',
+		   'Tabantha Wheat',
+		   4, 
+		   (SELECT * FROM OPENROWSET(BULK N'C:\Users\jorda\Documents\BOTWImages\Pumpkinsoup.jpg', SINGLE_BLOB) as T1)
+);
+
+SELECT RecipeName,
+	Ingredient1,
+	Ingredient2,
+	Ingredient3,
+	Ingredient4,
+	Num_Hearts,
+	Recipe_Image
+FROM Botw_Recipes
+WHERE RecipeName = 'Pumpkin stew'
